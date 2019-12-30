d11313c567331fb76c2d02fb517321bb36800d40
Bien Nguyen <biendltb@gmail.com>
2019-12-30 18:43:26 +0700

    TEST: count the external loss (>2.5 sigma)

--- src/models/gauface.py
+++ src/models/gauface.py
@@ -6,7 +6,7 @@ from src.datasets.face_datasets import load_ds
 
 
 class GauFace:
-    def __init__(self, dataset_fn: Callable = load_ds, network_fn: Callable = gauface_vnet.resnext_net,
+    def __init__(self, dataset_fn: Callable = load_ds, network_fn: Callable = gauface_dla.dla_lite_net,
                  lr: float = 1e-4,
                  dataset_args: Dict = None):
 
@@ -23,12 +23,15 @@ class GauFace:
         out_map = self.model(im)
 
         # filter the heatmap in the range of 2.5 sigma
-        mask = tf.greater_equal(heat_map, tf.exp(-25.0/4))
+        in_mask = tf.greater_equal(heat_map, tf.exp(-25.0/4))
+        ex_mask = tf.math.logical_not(in_mask)
+
         diff_tensor = tf.abs(out_map - heat_map)
-        out = tf.boolean_mask(diff_tensor, mask)
-        loss = tf.reduce_mean(out)
+
+        in_loss = tf.reduce_mean(tf.boolean_mask(diff_tensor, in_mask))
+        ex_loss = tf.reduce_mean(tf.boolean_mask(diff_tensor, ex_mask))
 
         # use L2 loss
         # loss = tf.reduce_mean(tf.abs(out_map - heat_map))
 
-        return loss
+        return in_loss + ex_loss
